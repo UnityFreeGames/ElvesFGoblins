fa6997cec62b3f47f3097803541c4e012cd4fde2
mosisedi420 <83016119+mosisedi420@users.noreply.github.com>
2022-05-25 06:04:13 +0430

    Update App.js

--- src/App.js
+++ src/App.js
@@ -231,7 +231,7 @@ function App() {
                 color: "var(--accent-text)",
               }}
             >
-               {data.totalSupply} / 3437
+               {data.totalSupply} / 3777
             </s.TextTitle>
             <s.TextDescription
               style={{
